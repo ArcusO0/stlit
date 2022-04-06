24d01fbac5fd3d109d55af97dfa87dc0cd2d4aae
Arcus0 <65378353+ArcusO0@users.noreply.github.com>
2022-04-06 11:43:06 +0800

    Update whencome.py

--- whencome.py
+++ whencome.py
@@ -8,6 +8,9 @@ import math
 from math import radians ,cos, sin, asin, sqrt
 import streamlit as st
 import os 
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 def process_time(times):
@@ -44,6 +47,18 @@ for i in range(10):
     busstopdata = pd.concat([busstopdata,pd.DataFrame.from_dict(bus_stops.json()['value'])])
 busstopdata = busstopdata.set_index('BusStopCode')
 print(busstopdata)
+print(busstopdata.index['Description' == busstopdata.iloc[i][1]])
+closeby = [' ']
+closebyent = [' ']
+closebynum = [' ']
+def busupdate(name):
+    for i in range(len(closebyent)):
+        if name == closeby[i][0]:
+            st.table(getbus(closeby[i][1]))
+def busupdatenum(num):
+    st.table(getbus(num))
+location = requests.get('http://ip-api.com/json/')
+print(location.json())
 loc_button = Button(label="Get Location")
 loc_button.js_on_event("button_click", CustomJS(code="""
     navigator.geolocation.getCurrentPosition(
@@ -59,27 +74,16 @@ result = streamlit_bokeh_events(
     refresh_on_update=False,
     override_height=75,
     debounce_time=0)
-lat = 0 
+lat = 0
 lon = 0
 if result:
     if "GET_LOCATION" in result:
-        print(result.get("GET_LOCATION"))
-    lat = result.get("GET_LOCATION")['lat']
-    lon = result.get("GET_LOCATION")['lon']
-print(busstopdata.index['Description' == busstopdata.iloc[i][1]])
-closeby = [' ']
-closebyent = [' ']
-closebynum = [' ']
-def busupdate(name):
-    for i in range(len(closebyent)):
-        if name == closeby[i][0]:
-            st.table(getbus(closeby[i][1]))
-def busupdatenum(num):
-    st.table(getbus(num))
+        lat = result.get("GET_LOCATION")['lat']
+        lon = result.get("GET_LOCATION")['lon']
 
 if lat != 0 and lon != 0:
     for i in range(busstopdata.shape[0]):
-        coords1=(float(lat),float(lon))
+        coords1=(lat,lon)
         coords2 = (float(busstopdata.iloc[i][2]),float(busstopdata.iloc[i][3]))
         lon1 ,lat1,lon2,lat2 = map(radians,[coords1[1],coords1[0],coords2[1],coords2[0]])
         dlon = lon2 - lon1 
