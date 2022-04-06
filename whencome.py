7722707111ed93130d910f67f9854dd1851b3577
Arcus0 <65378353+ArcusO0@users.noreply.github.com>
2022-04-06 20:25:41 +0800

    Update whencome.py

--- whencome.py
+++ whencome.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import numpy as np
 import time
-from datetime import datetime
+from datetime import datetime,timedelta
 import math
 from math import radians ,cos, sin, asin, sqrt
 import streamlit as st
@@ -13,7 +13,7 @@ from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 def process_time(times):
     if times != '':
-        now = datetime.now()
+        now = datetime.now()+timedelta(hours=-8)
         now = datetime.strptime(datetime.strftime(now,"%H:%M:%S"),"%H:%M:%S")
         arrival_time = datetime.strptime(times,"%H:%M:%S")
         if arrival_time < now:
