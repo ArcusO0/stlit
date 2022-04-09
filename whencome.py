4fa3db480d76f89c6fce71efaccda5e08b1a9097
Arcus0 <65378353+ArcusO0@users.noreply.github.com>
2022-04-09 21:28:55 +0800

    Update whencome.py

--- whencome.py
+++ whencome.py
@@ -12,6 +12,27 @@ from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 import pytz
 from datetime import datetime
+def select(ops,bus,data):
+    if ops == []:
+        return '-'
+    else:
+        text = ''
+        if 'Bus operator' in ops:
+            text += data.at['OriginCode','Operator']
+        if 'Seats available' in ops and len(text) != 1:
+            text += ' '+data.at['Load','NextBus']
+        elif 'Seats available' in ops and len(text) < 1:
+            text += data.at['Load','NextBus']
+        if 'Vehicle type' in ops and len(text) != 1:
+            text += ' '+data.at['Type',bus].replace("DD","2-deck").replace("SD","1-deck").replace("BD","Bendy")
+        elif 'Vehicle type' in ops and len(text) < 1:
+            text += data.at['Type',bus].replace("DD","2-deck").replace("SD","1-deck").replace("BD","Bendy")
+        if 'Wheel-chair accessibility' in ops and len(text) != 1:
+            text += ' '+data.at['Feature','NextBus']
+        elif 'Wheel-chair accessibility' in ops and len(text) < 1:
+            text += data.at['Feature','NextBus']
+        return(text)
+
 def process_time(times):
     if times != '':
         now = datetime.now(pytz.timezone('Asia/Singapore'))
@@ -33,18 +54,16 @@ def process_time(times):
 info = []
 def getbus(code):
     partinfo = []    
-    response = requests.get("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode="+str(code),headers = { 'AccountKey' : 'qV1hBipQTZiK4AHSYmS92Q==', 'accept' : 'application/json'})
+    response = requests.get("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode="+str(code),headers = { 'AccountKey' : st.secrets['key'], 'accept' : 'application/json'})
     for i in range(len(response.json()['Services'])):
-        
         df = pd.DataFrame.from_dict(response.json()['Services'][i])
-        partinfo = ([df.at['OriginCode','ServiceNo'],process_time(df.at['EstimatedArrival','NextBus'][11:19]),df.at['Type','NextBus'].replace("DD","2-deck").replace("SD","1-deck").replace("BD","Bendy")+' '+df.at['Feature','NextBus']+' '+df.at['Load','NextBus'],process_time(df.at['EstimatedArrival','NextBus2'][11:19]),df.at['Type','NextBus2'].replace("DD","2-deck").replace("SD","1-deck").replace("BD","Bendy")+' '+df.at['Feature','NextBus2']+' '+df.at['Load','NextBus2'],process_time(df.at['EstimatedArrival','NextBus3'][11:19]),df.at['Type','NextBus3'].replace("DD","2-deck").replace("SD","1-deck").replace("BD","Bendy")+' '+df.at['Feature','NextBus3']+' '+df.at['Load','NextBus3']])
+        partinfo = ([df.at['OriginCode','ServiceNo'],process_time(df.at['EstimatedArrival','NextBus'][11:19]),select(options,'NextBus',df),process_time(df.at['EstimatedArrival','NextBus2'][11:19]),select(options,'NextBus2',df),process_time(df.at['EstimatedArrival','NextBus3'][11:19]),select(options,'NextBus3',df)])
         info.append(partinfo)
     myarray = np.array(info)
     bustimes = pd.DataFrame(myarray,columns=['Bus Number','1st Bus ETA(MM:SS)','1st Bus type','2nd Bus ETA(MM:SS)','2nd Bus type','3rd Bus ETA(MM:SS)','3rd Bus type'])
     bustimes = bustimes.set_index('Bus Number')
     bustimes = bustimes.sort_index()
     return(bustimes)
-    
 busstopdata = pd.DataFrame()
 busstopdata = pd.read_excel(open("bus_stops.xlsx",'rb'),sheet_name='bus_stops')
 busstopdata = busstopdata.set_index('BusStopCode')
@@ -82,7 +101,7 @@ if lat != 0 and lon != 0:
         dlon = lon2 - lon1 
         dlat = lat2 - lat1
         #geopy.distance.geodesic(coords1, coords2).km
-        if 6371*2*asin(sqrt(sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2))<1.0:
+        if 6371*2*asin(sqrt(sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2))<0.5:
             closeby.append([busstopdata.iloc[i][1]+' ('+str(busstopdata[busstopdata['Description']==busstopdata.iloc[i][1]].index[0])+')',busstopdata[busstopdata['Description']==busstopdata.iloc[i][1]].index[0]])
             closebyent.append(busstopdata.iloc[i][1]+' ('+str(busstopdata[busstopdata['Description']==busstopdata.iloc[i][1]].index[0])+')')
             dist.append(6371*2*asin(sqrt(sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2)))
@@ -95,11 +114,25 @@ if lat != 0 and lon != 0:
     busstopdf = pd.DataFrame(data=busstops)
     busstopdf['Distance'] = pd.to_numeric(busstopdf['Distance'])
     busstopdf = busstopdf.sort_values("Distance")
-    st.map(plots,zoom = 14)
+    options = st.multiselect('Select the info to show:',['Bus operator','Seats available','Vehicle type','Wheel-chair accessibility'])
+    print(options)
     option = st.selectbox('Select the name of the bus stop',busstopdf['Name'])
     if option != ' ':
-        st.write('You selected:', option)  
-        st.write('Legend: WAB: Wheel-chair accessible \nSEA: seats available\nSDA: Standing available\nLSD: Limited standing')
+        st.write('You selected:', option)
+        if 'Bus operator' in options:
+            st.write('Bus operators:')
+            st.write('SBST: SBS Transit')
+            st.write('SMRT: SMRT Corporation')
+            st.write('TTS: Tower Transit Singapore')
+            st.write('GAS: Go Ahead Singapore')
+        if  'Seats available'in options or'Wheel-chair accessibility' in options:
+            optiontext = 'Legend:'
+            if 'Seats available'in options:
+                optiontext += ' SEA: seats available SDA: Standing available LSD: Limited standing'
+            if 'Wheel-chair accessibility' in options:
+                optiontext += ' WAB: Wheel-chair accessible'
+            st.write(optiontext)
         st.button(label='Refresh',on_click=busupdate(option))
     else:
         st.write("You haven't selected anything")
+    st.map(plots,zoom = 14)
